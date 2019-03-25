e1fde94f94fe5a99ba58d732e96469a132d6e6c5
Kamil Kokot <kamil@kokot.me>
2019-03-25 16:20:49 +0100

    Make taxons export great again

--- src/Exporter/Plugin/TaxonResourcePlugin.php
+++ src/Exporter/Plugin/TaxonResourcePlugin.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace FriendsOfSylius\SyliusImportExportPlugin\Exporter\Plugin;
 
 use Sylius\Component\Taxonomy\Model\TaxonInterface;
+use Sylius\Component\Taxonomy\Model\TaxonTranslationInterface;
 
 final class TaxonResourcePlugin extends ResourcePlugin
 {
@@ -18,8 +19,20 @@ final class TaxonResourcePlugin extends ResourcePlugin
         /** @var TaxonInterface $resource */
         foreach ($this->resources as $resource) {
             if (null !== $resource->getParent()) {
-                $this->addDataForResource($resource, 'Parent', $resource->getParent()->getId());
+                $this->addDataForResource($resource, 'Parent', $resource->getParent()->getCode());
             }
+
+            $translations = [];
+            /** @var TaxonTranslationInterface $translation */
+            foreach ($resource->getTranslations() as $translation) {
+                $translations[$translation->getLocale()] = [
+                    'Name' => $translation->getName(),
+                    'Slug' => $translation->getSlug(),
+                    'Description' => $translation->getDescription(),
+                ];
+            }
+
+            $this->addDataForResource($resource, 'Translations', $translations);
         }
     }
 }
