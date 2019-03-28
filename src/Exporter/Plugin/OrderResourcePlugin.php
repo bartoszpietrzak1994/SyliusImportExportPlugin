f523e0f860e0d9a169a4dabdc8cd123fb641d37c
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2019-03-28 12:24:01 +0100

    Legacy order resource plugin removed

--- src/Exporter/Plugin/OrderResourcePlugin.php
+++ src/Exporter/Plugin/OrderResourcePlugin.php
@@ -4,38 +4,24 @@ declare(strict_types=1);
 
 namespace FriendsOfSylius\SyliusImportExportPlugin\Exporter\Plugin;
 
-use Doctrine\ORM\EntityManagerInterface;
-use FriendsOfSylius\SyliusImportExportPlugin\Exporter\ORM\Hydrator\HydratorInterface;
-use FriendsOfSylius\SyliusImportExportPlugin\Service\AddressConcatenationInterface;
+use Doctrine\Common\Collections\Collection;
+use Sylius\Component\Core\Model\AddressInterface;
+use Sylius\Component\Core\Model\AdjustmentInterface;
+use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\OrderItemInterface;
-use Sylius\Component\Product\Model\ProductInterface;
-use Sylius\Component\Product\Model\ProductVariantInterface;
-use Sylius\Component\Resource\Model\ResourceInterface;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
-use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Sylius\Component\Core\Model\OrderItemUnitInterface;
+use Sylius\Component\Core\Model\PaymentInterface;
+use Sylius\Component\Core\Model\PromotionCouponInterface;
+use Sylius\Component\Core\Model\PromotionInterface;
+use Sylius\Component\Order\Model\AdjustableInterface;
+use Sylius\Component\Promotion\Model\PromotionActionInterface;
+use Sylius\Component\Promotion\Model\PromotionRuleInterface;
+use Sylius\Component\Resource\Model\CodeAwareInterface;
+use Sylius\Component\Shipping\Model\ShipmentInterface;
 
-class OrderResourcePlugin extends ResourcePlugin
+final class OrderResourcePlugin extends ResourcePlugin
 {
-    /** @var AddressConcatenationInterface */
-    private $addressConcatenation;
-
-    /** @var HydratorInterface */
-    private $orderHydrator;
-
-    public function __construct(
-        RepositoryInterface $repository,
-        PropertyAccessorInterface $propertyAccessor,
-        EntityManagerInterface $entityManager,
-        AddressConcatenationInterface $addressConcatenation,
-        HydratorInterface $orderHydrator
-    ) {
-        parent::__construct($repository, $propertyAccessor, $entityManager);
-
-        $this->addressConcatenation = $addressConcatenation;
-        $this->orderHydrator = $orderHydrator;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -45,103 +31,316 @@ class OrderResourcePlugin extends ResourcePlugin
 
         /** @var OrderInterface $resource */
         foreach ($this->resources as $resource) {
-            // insert customer information to specific fields
-            $this->addCustomerData($resource);
-
-            // insert shippingaddress to the specific field
-            $this->addShippingAddressData($resource);
-
-            // insert billingaddress to the specific field
-            $this->addBillingAddressData($resource);
-
-            $items = $this->getItemsAndCount($resource);
-
-            $this->addOrderItemData($items, $resource);
-        }
-    }
-
-    private function addCustomerData(OrderInterface $resource): void
-    {
-        $customer = $resource->getCustomer();
-
-        if (null === $customer) {
-            return;
-        }
-
-        $this->addDataForResource($resource, 'Gender', $customer->getGender());
-        $this->addDataForResource($resource, 'Full_name', $customer->getFullName());
-        $this->addDataForResource($resource, 'Telephone', $customer->getPhoneNumber());
-        $this->addDataForResource($resource, 'Email', $customer->getEmail());
-    }
-
-    private function addShippingAddressData(OrderInterface $resource): void
-    {
-        $shippingAddress = $resource->getShippingAddress();
-
-        if (null === $shippingAddress) {
-            return;
-        }
-
-        $shippingInfoString = $this->addressConcatenation->getString($shippingAddress);
-
-        $this->addDataForResource($resource, 'Shipping_address', $shippingInfoString);
-    }
-
-    private function addBillingAddressData(OrderInterface $resource): void
-    {
-        $billingAddress = $resource->getBillingAddress();
-
-        if (null === $billingAddress) {
-            return;
-        }
-
-        $billingInfoString = $this->addressConcatenation->getString($billingAddress);
-
-        $this->addDataForResource($resource, 'Billing_address', $billingInfoString);
-    }
-
-    private function getItemsAndCount(OrderInterface $resource): array
-    {
-        $items = [];
-
-        /** @var OrderItemInterface $orderItem */
-        foreach ($resource->getItems() as $orderItem) {
-            /** @var ProductVariantInterface $variant */
-            $variant = $orderItem->getVariant();
-            /** @var ProductInterface $product */
-            $product = $variant->getProduct();
-
-            if (!isset($items[$product->getId()])) {
-                $items[$product->getId()] = [
-                    'name' => $product->getName(),
-                    'count' => 0,
-                ];
+            if (!$resource->getItems()->isEmpty()) {
+                $this->addOrderItems($resource);
             }
-            $items[$product->getId()]['count'] += $orderItem->getQuantity();
-        }
 
-        return $items;
-    }
-
-    private function addOrderItemData(array $items, OrderInterface $resource): void
-    {
-        $str = '';
-
-        foreach ($items as $itemId => $item) {
-            if (!empty($str)) {
-                $str .= ' | ';
+            if (!$resource->getPromotions()->isEmpty()) {
+                $this->addPromotions($resource);
             }
-            $str .= sprintf('%dx %s(id:%d)', $item['count'], $item['name'], $itemId);
-        }
 
-        $this->addDataForResource($resource, 'Product_list', $str);
+            if (!$resource->getPayments()->isEmpty()) {
+                $this->addPayments($resource);
+            }
+
+            if (!$resource->getShipments()->isEmpty()) {
+                $this->addShipments($resource);
+            }
+
+            if (!$resource->getAdjustments()->isEmpty()) {
+                $this->addAdjustments($resource);
+            }
+
+            if (null !== $resource->getCustomer()) {
+                $this->addCustomer($resource);
+            }
+
+            if (null !== $resource->getBillingAddress()) {
+                $this->addBillingAddress($resource);
+            }
+
+            if (null !== $resource->getShippingAddress()) {
+                $this->addShippingAddress($resource);
+            }
+
+            if (null !== $resource->getPromotionCoupon()) {
+                $this->addPromotionCoupon($resource);
+            }
+
+            $this->addDataForResource($resource, 'Channel', $this->getPossibleResourceCodeValue($resource->getChannel()));
+        }
     }
 
-    protected function findResources(array $idsToExport): array
+    private function addOrderItems(OrderInterface $order): void
     {
-        /** @var ResourceInterface[] $items */
-        $items = $this->orderHydrator->getHydratedResources($idsToExport);
+        $orderItemsToExport = [];
 
-        return $items;
+        /** @var Collection|OrderItemInterface[] $orderItems */
+        $orderItems = $order->getItems();
+
+        foreach ($orderItems as $orderItem) {
+            $orderItemsToExport[] = [
+                'Quantity' => $orderItem->getQuantity(),
+                'UnitPrice' => $orderItem->getUnitPrice(),
+                'Total' => $orderItem->getTotal(),
+                'Immutable' => $orderItem->isImmutable(),
+                'Units' => $this->getOrderItemUnits($orderItem),
+                'Adjustments' => $this->getAdjustments($orderItem),
+                'Product' => $this->getPossibleResourceCodeValue($orderItem->getProduct()),
+                'Variant' => $this->getPossibleResourceCodeValue($orderItem->getVariant()),
+            ];
+        }
+
+        $this->addDataForResource($order, 'Items', $orderItemsToExport);
+    }
+
+    private function getOrderItemUnits(OrderItemInterface $orderItem): array
+    {
+        $orderItemUnitsToExport = [];
+
+        /** @var Collection|OrderItemUnitInterface[] $orderItemUnits */
+        $orderItemUnits = $orderItem->getUnits();
+
+        foreach ($orderItemUnits as $orderItemUnit) {
+            $orderItemUnitsToExport[] = [
+                'Total' => $orderItemUnit->getTotal(),
+                'Shipment' => $this->getShipmentAsArray($orderItemUnit->getShipment()),
+                'Adjustments' => $this->getAdjustments($orderItemUnit),
+            ];
+        }
+
+        return $orderItemUnitsToExport;
+    }
+
+    private function getAdjustments(AdjustableInterface $adjustable): array
+    {
+        $adjustmentsToExport = [];
+
+        /** @var Collection|AdjustmentInterface[] $adjustments */
+        $adjustments = $adjustable->getAdjustments();
+
+        foreach ($adjustments as $adjustment) {
+            $adjustmentsToExport[] = [
+                'Type' => $adjustment->getType(),
+                'Label' => $adjustment->getLabel(),
+                'Amount' => $adjustment->getAmount(),
+                'Neutral' => $adjustment->isNeutral(),
+                'Locked' => $adjustment->isLocked(),
+                'Charge' => $adjustment->isCharge(),
+                'Credit' => $adjustment->isCredit(),
+                'OriginCode' => $adjustment->getOriginCode()
+            ];
+        }
+
+        return $adjustmentsToExport;
+    }
+
+    private function getShipmentAsArray(ShipmentInterface $shipment): array
+    {
+        return [
+            'State' => $shipment->getState(),
+            'Method' => $this->getPossibleResourceCodeValue($shipment->getMethod()),
+            'Tracking' => $shipment->getTracking(),
+            'Tracked' => $shipment->isTracked(),
+        ];
+    }
+
+    private function addPromotions(OrderInterface $order): void
+    {
+        $promotionsToExport = [];
+
+        /** @var Collection|PromotionInterface[] $adjustments */
+        $promotions = $order->getPromotions();
+
+        foreach ($promotions as $promotion) {
+            $promotionsToExport[] = [
+                'Name' => $promotion->getName(),
+                'Description' => $promotion->getDescription(),
+                'Priority' => $promotion->getPriority(),
+                'Exclusive' => $promotion->isExclusive(),
+                'UsageLimit' => $promotion->getUsageLimit(),
+                'Used' => $promotion->getUsed(),
+                'CouponBased' => $promotion->isCouponBased(),
+                'Coupons' => $this->getPromotionCoupons($promotion),
+                'Rules' => $this->getPromotionRules($promotion),
+                'Actions' => $this->getPromotionActions($promotion),
+            ];
+        }
+
+        $this->addDataForResource($order, 'Promotions', $promotionsToExport);
+    }
+
+    private function getPromotionCoupons(PromotionInterface $promotion): array
+    {
+        $promotionCouponsToExport = [];
+
+        /** @var Collection|PromotionCouponInterface[] $adjustments */
+        $promotionCoupons = $promotion->getCoupons();
+
+        foreach ($promotionCoupons as $promotionCoupon) {
+            $promotionCouponsToExport[] = [
+                'UsageLimit' => $promotionCoupon->getUsageLimit(),
+                'Used' => $promotionCoupon->getUsed(),
+                'ExpiresAt' => null !== $promotionCoupon->getExpiresAt() ? $promotionCoupon->getExpiresAt()->format('Y-m-d H:i:s') : null,
+                'Valid' => $promotionCoupon->isValid(),
+            ];
+        }
+
+        return $promotionCouponsToExport;
+    }
+
+    private function getPromotionRules(PromotionInterface $promotion): array
+    {
+        $promotionRulesToExport = [];
+
+        /** @var Collection|PromotionRuleInterface[] $adjustments */
+        $promotionRules = $promotion->getRules();
+
+        foreach ($promotionRules as $promotionRule) {
+            $promotionRulesToExport[] = [
+                'Type' => $promotionRule->getType(),
+                'Configuration' => $promotionRule->getConfiguration(),
+            ];
+        }
+
+        return $promotionRulesToExport;
+    }
+
+    private function getPromotionActions(PromotionInterface $promotion): array
+    {
+        $promotionActionsToExport = [];
+
+        /** @var Collection|PromotionActionInterface[] $adjustments */
+        $promotionActions = $promotion->getActions();
+
+        foreach ($promotionActions as $promotionAction) {
+            $promotionActionsToExport[] = [
+                'Type' => $promotionAction->getType(),
+                'Configuration' => $promotionAction->getConfiguration()
+            ];
+        }
+
+        return $promotionActionsToExport;
+    }
+
+    private function addPayments(OrderInterface $order): void
+    {
+        $paymentsToExport = [];
+
+        /** @var Collection|PaymentInterface[] $payments */
+        $payments = $order->getPayments();
+
+        foreach ($payments as $payment) {
+            $paymentsToExport[] = [
+                'PaymentMethod' => $this->getPossibleResourceCodeValue($payment->getMethod()),
+                'State' => $payment->getState(),
+                'CurrencyCode' => $payment->getCurrencyCode(),
+                'Amount' => $payment->getAmount(),
+                'Details' => $payment->getDetails()
+            ];
+        }
+
+        $this->addDataForResource($order, 'Payments', $paymentsToExport);
+    }
+
+    private function addShipments(OrderInterface $order): void
+    {
+        $shipmentsToExport = [];
+
+        /** @var Collection|ShipmentInterface[] $payments */
+        $shipments = $order->getShipments();
+
+        foreach ($shipments as $shipment) {
+            $shipmentsToExport[] = $this->getShipmentAsArray($shipment);
+        }
+
+        $this->addDataForResource($order, 'Shipments', $shipmentsToExport);
+    }
+
+    private function addCustomer(OrderInterface $order): void
+    {
+        /** @var CustomerInterface $customer */
+        $customer = $order->getCustomer();
+
+        $customerToExport = [
+            'Email' => $customer->getEmail(),
+            'EmailCanonical' => $customer->getEmailCanonical(),
+            'FirstName' => $customer->getFirstName(),
+            'LastName' => $customer->getLastName(),
+            'Birthday' => null !== $customer->getBirthday() ? $customer->getBirthday()->format('Y-m-d H:i:s'): null,
+            'Gender' => $customer->getGender(),
+            'Group' => null !== $customer->getGroup() ? $customer->getGroup()->getName() : null,
+            'PhoneNumber' => $customer->getPhoneNumber(),
+            'SubscribedToNewsletter' => $customer->isSubscribedToNewsletter(),
+            'DefaultAddress' => $this->getAddressAsArray($customer->getDefaultAddress()),
+        ];
+
+        $this->addDataForResource($order, 'Customer', $customerToExport);
+    }
+
+    private function addBillingAddress(OrderInterface $order): void
+    {
+        /** @var AddressInterface $address */
+        $address = $order->getBillingAddress();
+
+        $addressToExport = $this->getAddressAsArray($address);
+
+        $this->addDataForResource($order, 'BillingAddress', $addressToExport);
+    }
+
+    private function addShippingAddress(OrderInterface $order): void
+    {
+        /** @var AddressInterface $address */
+        $address = $order->getShippingAddress();
+
+        $addressToExport = $this->getAddressAsArray($address);
+
+        $this->addDataForResource($order, 'ShippingAddress', $addressToExport);
+    }
+
+    private function getAddressAsArray(?AddressInterface $address): array
+    {
+        if (null === $address) {
+            return [];
+        }
+
+        return [
+            'FirstName' => $address->getFirstName(),
+            'LastName' => $address->getLastName(),
+            'PhoneNumber' => $address->getPhoneNumber(),
+            'Company' => $address->getCompany(),
+            'CountryCode' => $address->getCountryCode(),
+            'ProvinceCode' => $address->getProvinceCode(),
+            'ProvinceName' => $address->getProvinceName(),
+            'Street' => $address->getStreet(),
+            'City' => $address->getCity(),
+            'Postcode' => $address->getPostcode(),
+        ];
+    }
+
+    private function addAdjustments(OrderInterface $order): void
+    {
+        $adjustmentsToExport = $this->getAdjustments($order);
+
+        $this->addDataForResource($order, 'Adjustments', $adjustmentsToExport);
+    }
+
+    private function addPromotionCoupon(OrderInterface $order): void
+    {
+        /** @var PromotionCouponInterface $promotionCoupon */
+        $promotionCoupon = $order->getPromotionCoupon();
+
+        $promotionCouponToExport = [
+            'UsageLimit' => $promotionCoupon->getUsageLimit(),
+            'Used' => $promotionCoupon->getUsed(),
+            'ExpiresAt' => null !== $promotionCoupon->getExpiresAt() ? $promotionCoupon->getExpiresAt()->format('Y-m-d H:i:s') : null,
+        ];
+
+        $this->addDataForResource($order, 'PromotionCoupon', $promotionCouponToExport);
+    }
+
+    private function getPossibleResourceCodeValue(?CodeAwareInterface $codeAware): ?string
+    {
+        return null !== $codeAware ? $codeAware->getCode() : null;
     }
 }
